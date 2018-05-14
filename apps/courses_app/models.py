7ddb9717bd0bdf4f3b666753ca1be130dda72718
RAINFOREST\Gustavo <gustavomonardez@gmail.com>
2018-05-14 11:08:03 -0700

    created course model and course validation manager

--- apps/courses_app/models.py
+++ apps/courses_app/models.py
@@ -1,3 +1,20 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class CourseManager(models.Manager):
+    def validator(self,data):
+        errors = {}
+        if len(data["name"]) < 6:
+            errors["name"] = "Course name has to be longer than 5 characters"
+        if len(data["description"]) < 16:
+            errors["description"] = "Course description has to be longer than 15 characters"
+        return errors
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = CourseManager()
+
