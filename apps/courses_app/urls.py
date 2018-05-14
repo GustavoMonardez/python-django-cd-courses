8e9fe9e75be8f0559761641b550359ea780fb9df
RAINFOREST\Gustavo <gustavomonardez@gmail.com>
2018-05-14 10:57:58 -0700

    added views functions

--- apps/courses_app/urls.py
+++ apps/courses_app/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.index)
+    url(r"^$", views.index),
+    url(r"^courses/new$", views.new),
+    url(r"^courses/destroy/(?P<course_id>\d+)$", views.confirm),
+    url(r"^courses/destroy/(?P<course_id>\d+)/confirm$", views.destroy)
 ]
--- apps/courses_app/views.py
+++ apps/courses_app/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return render(request, "courses_app/index.html")
+
+def new(request):
+    # capture data
+    name = request.POST["name"]
+    description = request.POST["description"]
+    # validate data (name > 5 and desc > 15)
+
+    # insert data
+
+    # redirect back to home page
+    print("just added a new course")
+    return redirect("/")
+
+def confirm(request,course_id):
+    print("confirm page:", course_id)
+    course = {"course_id": course_id}
+    return render(request, "courses_app/confirm.html",course)
+
+def destroy(request, course_id):
+    print('destroy has been confimed: ', course_id)
+    return redirect("/")
